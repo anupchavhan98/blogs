912312e20f40f26b97f1d130185a5a82f96e1ee4
kewal <kewaljangilwar7@gmail.com>
2025-06-30 17:32:48 +0530

    create view of user

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user');
+    return view('welcome');
 });
 
-// Route::get('/user',function(){
-//     return view('user');
-// });
+Route::view('/user','user');
